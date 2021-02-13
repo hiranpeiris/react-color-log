418da80fa3b29a10b9085af33bed9b8e6775181d
Manohar Reddy Poreddy <manoharporeddy@outlook.com>
2021-02-13 12:09:53 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -6,16 +6,16 @@ export const _success = (msg) => {
 };
 
 export const _info = (msg) => {
-  console.log(`%c${msg}`, `color:${COLORS.INFO};font-size: ${FONT_SIZE}; font-weight: ${FONT_WEIGHT}`);
+  console.info(`%c${msg}`, `color:${COLORS.INFO};font-size: ${FONT_SIZE}; font-weight: ${FONT_WEIGHT}`);
   return 'info';
 };
 
 export const _warning = (msg) => {
-  console.log(`%c${msg}`, `color:${COLORS.WARNING};font-size: ${FONT_SIZE}; font-weight: ${FONT_WEIGHT}`);
+  console.warn(`%c${msg}`, `color:${COLORS.WARNING};font-size: ${FONT_SIZE}; font-weight: ${FONT_WEIGHT}`);
   return 'warning';
 };
 
 export const _error = (msg) => {
-  console.log(`%c${msg}`, `color:${COLORS.ERROR};font-size: ${FONT_SIZE}; font-weight: ${FONT_WEIGHT}`);
+  console.error(`%c${msg}`, `color:${COLORS.ERROR};font-size: ${FONT_SIZE}; font-weight: ${FONT_WEIGHT}`);
   return 'error';
 };
