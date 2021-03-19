ace16920ef8caae1812e8854fec2ec69648531e9
H.R. Hiran Peiris <hiranpeiris.sl@gmail.com>
2021-03-19 22:48:38 +0530

    bug fixes

--- src/index.js
+++ src/index.js
@@ -13,16 +13,16 @@ export const _success = (msg) => {
 };
 
 export const _info = (msg) => {
-  console.info(`%c${msg}`, `color:${COLORS.INFO};font-size: ${FONT_SIZE}; font-weight: ${FONT_WEIGHT}`);
+  console.log(`%c${msg}`, `color:${COLORS.INFO};font-size: ${FONT_SIZE}; font-weight: ${FONT_WEIGHT}`);
   return 'info';
 };
 
 export const _warning = (msg) => {
-  console.warn(`%c${msg}`, `color:${COLORS.WARNING};font-size: ${FONT_SIZE}; font-weight: ${FONT_WEIGHT}`);
+  console.log(`%c${msg}`, `color:${COLORS.WARNING};font-size: ${FONT_SIZE}; font-weight: ${FONT_WEIGHT}`);
   return 'warning';
 };
 
 export const _error = (msg) => {
-  console.error(`%c${msg}`, `color:${COLORS.ERROR};font-size: ${FONT_SIZE}; font-weight: ${FONT_WEIGHT}`);
+  console.log(`%c${msg}`, `color:${COLORS.ERROR};font-size: ${FONT_SIZE}; font-weight: ${FONT_WEIGHT}`);
   return 'error';
 };
